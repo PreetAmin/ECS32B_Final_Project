9288aae6d0ed290b824892a9bb25954a781b2fe6
Dave <59101488+fewvax@users.noreply.github.com>
2020-11-21 17:28:17 -0800

    Update framework.py
    Added Delivery by address code

--- framework.py
+++ framework.py
@@ -2,58 +2,65 @@ class Package:
     def __init__(self, id):
         self.id = id
         self.address = ""
-        self.office = ""
         self.ownerName = ""
         self.collected = False
         self.delivered = False
-       
-        
+
+
 class Truck:
-    def __init__(self, id, n, loc):
+    def __init__(self, id, n):
         self.id = id
         self.size = n
-        self.location = loc
+        self.location = ""
         self.packages = {}
 
-    def collectPackage(self, pk):
-        #Add package to packages dictionary
+    def addPackage(self, pk):
+        # Add package to packages dictionary
         self.packages[pk.id] = pk
-        
-        #Set package pickedUp to true 
-        pk.pickedUp = True 
-       
-        
-    def deliverPackage(self, pk):#Milap
-        #Find delivery location
-        #drive to required location
-        #remove the package from truck
-        #set delivery status to true for package  
 
-    def deliverPackageByAddress(self, addr):#Dave
-        #drive to address 
-        #find all the packages we need to deliver and their names to some DS 
-        #loop through all needed packages 
-            #remove package from truck
-            #set package status to delivered 
+        # Set package pickedUp to true
+        pk.collected = True
 
-            
-            
-    def removePackage(self, pk, office):#Preet
-        #drive to post office 
-        #remove package from truck (don't set delivery status to true)
-        #set picked up back to false 
+    def deliverPackage(self, pk):  # Milap
 
-    def driveTo(self, loc1, loc2):#Preet 
-        #set truck's location to location 2 
-        
-        
+    # Find delivery location
+    # drive to required location
+    # remove the package from truck
+    # set delivery status to true for package
 
-    def getPackagesIds(self):#Preet 
-        #intialize empty python list
-        #for package on truck:
-            #get ID 
-            #add ID to list 
-            
+    def deliverPackageByAddress(self, addr):  # Dave
+    # drive to address
+        self.driveTo(self.location,addr)
+    # find all the packages we need to deliver
+    # loop through all needed packages
+        for key in self.packages:
+            if self.packages[key].address == addr:
+                # set package status to delivered
+                self.packages[key].deliver = True
+                # remove package from truck
+                #del self.packages[key]
+
+
+
+
+
+
+    def removePackage(self, pk, office):  # Preet
+
+    # drive to post office
+    # remove package from truck (don't set delivery status to true)
+    # set picked up back to false
+
+    def driveTo(self, loc1, loc2):  # Preet
+
+    # set truck's location to location 2
+
+    def getPackagesIds(self):  # Preet
+# intialize empty python list
+# for package on truck:
+# get ID
+# add ID to list
+
 
 
 
