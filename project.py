477e733763caa96c34594ec1600224189ebbf966
Dave <59101488+fewvax@users.noreply.github.com>
2020-11-23 16:33:22 -0800

    Update project.py

--- project.py
+++ project.py
@@ -16,7 +16,7 @@ class Truck:
         self.packages = {}
 
     def collectPackage(self, pk):
-        if self.location == pk.office:
+        if self.location == pk.office and len(self.packages) < self.size :
         # Add package to packages dictionary
             self.packages[pk.id] = pk
 
@@ -26,42 +26,46 @@ class Truck:
     def deliverPackage(self, pk):
         # Find delivery location
         # drive to required location
+        if pk.id in self.packages:
 
         # remove the package from truck
-        del self.packages[pk.id]
+            del self.packages[pk.id]
 
         # set delivery status to true for package
-        pk.delivered = True
+            pk.delivered = True
 
     def deliverPackageByAddress(self, addr):
         # drive to address
-        self.driveTo(self.location, addr)
-
         # find all the packages we need to deliver
         # loop through all needed packages
+        delivered_pack = []
         for key in self.packages:
             if self.packages[key].address == addr:
                 # set package status to delivered
                 self.packages[key].delivered = True
-
+                delivered_pack.append(key)
+        for key in delivered_pack:
+            if self.packages[key].delivered == True:
                 # remove package from truck
                 del self.packages[key]
 
+                
+
     def removePackage(self, pk, office):
         # drive to post office
-        self.driveTo(self.location, office)
-
         # set picked up back to false
-        self.packages[pk.id].collected = False
-
+        if self.location == office and pk.id in self.packages:
+            if self.packages[pk.id].office != office:
+                self.packages[pk.id].office = office
+            self.packages[pk.id].collected = False
         # remove package from truck (don't set delivery status to true)
-        del self.packages[pk.id]
+            del self.packages[pk.id]
 
     def driveTo(self, loc1, loc2):
         # set truck's location to location 2
-        if loc1 != loc2:
+        if self.location == loc1 and loc1 != loc2:
             self.location = loc2
-
+        
     def getPackagesIds(self):
         # intialize empty python list
         Id_list = []
